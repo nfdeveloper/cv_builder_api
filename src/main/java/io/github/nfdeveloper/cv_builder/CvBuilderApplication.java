5cd87c7bd61dacee914206df4072774a9005a8da
nfdeveloper <nfdeveloper04@gmail.com>
2024-10-29 11:09:34 -0300

    Criando projeto e configurando banco de dados

---
+++ src/main/java/io/github/nfdeveloper/cv_builder/CvBuilderApplication.java
@@ -0,0 +1,13 @@
+package io.github.nfdeveloper.cv_builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/nfdeveloper/cv_builder/CvBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nfdeveloper.cv_builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
